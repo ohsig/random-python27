f5ea7153897c5c4e7a33aced64d0aaa6f1a44dd9
caseybain <casey.bain@8451.com>
2016-09-17 17:08:29 -0400

    increased size of squares from 100 to 200

--- Drawing/circle_of_squares.py
+++ Drawing/circle_of_squares.py
@@ -2,7 +2,7 @@ import turtle
 
 def draw_square(some_turtle):
     for i in range(1,5):
-        some_turtle.forward(100)
+        some_turtle.forward(200)
         some_turtle.right(90)
 
 def draw_art():
